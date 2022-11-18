aad8dae1df18bf66df57eb3f3483a1ec14d94843
Texotek <texotek17@gmail.com>
2022-11-18 11:06:40 +0100

    asdf

--- lua/texotek/plugins/lsp/mason.lua
+++ lua/texotek/plugins/lsp/mason.lua
@@ -1,40 +1,39 @@
 local mason_status, mason = pcall(require, "mason")
 if not mason_status then
-    return
+	return
 end
 
 local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_lspconfig_status then
-    print("triggered")
-    return
+	return
 end
 
 local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
 if not mason_null_ls_status then
-    return
+	return
 end
 
 mason.setup()
 
 mason_lspconfig.setup({
-    ensure_installed = {
-        "bashls",
-        "clangd",
-        "pyright",
-        "cssls",
-        "html",
-        "jsonls",
-        "jdtls",
-        "tsserver",
-        "sumneko_lua",
-        "marksman",
-    }
+	ensure_installed = {
+		"bashls",
+		"clangd",
+		"pyright",
+		"cssls",
+		"html",
+		"jsonls",
+		"jdtls",
+		"tsserver",
+		"sumneko_lua",
+		"marksman",
+	},
 })
 
 mason_null_ls.setup({
-    ensure_installed = {
-        "prettier",
-        "pylint",
-        "stylua",
-    }
+	ensure_installed = {
+		"prettier",
+		"pylint",
+		"stylua",
+	},
 })
