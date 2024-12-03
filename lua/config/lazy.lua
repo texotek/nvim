9db281e6f72e070027ed1121bd91d1960bd68e3d
texotek <fdahmen@proton.me>
2024-12-03 14:42:09 +0100

    disable checker

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,6 +30,6 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = false },
 })
 
