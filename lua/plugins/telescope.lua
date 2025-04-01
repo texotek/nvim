ff62c3958ae49dae5b4fa560742c8f42292c1947
texotek <fdahmen@proton.me>
2025-04-01 16:39:48 +0200

    fix(telescope): move telescope out of dependencies

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,9 @@
 return {
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release', },
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
     dependencies = {
         'nvim-lua/plenary.nvim',
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release', },
         'andrew-george/telescope-themes',
     },
     config = function()
