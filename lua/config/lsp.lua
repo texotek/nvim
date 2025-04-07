24e89f3f5f7ddb0bde4b1bf012d08c228e3bd81e
texotek <fdahmen@proton.me>
2025-04-07 10:24:23 +0200

    mason: Remove rust_analyzer out of ensure_installed

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,6 +1,6 @@
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = { 'lua_ls', 'rust_analyzer' },
+    ensure_installed = { 'lua_ls' },
     handlers = {
         function(server_name)
             require('lspconfig')[server_name].setup({})
